64e87b95378458c9fb100e5ceb0dd7d0c2f5bceb
bailindouban <bailindouban@gmail.com>
2015-04-08 14:58:44 +0800

    Add to previous commit

--- smartgraph/src/main/java/originator/ailin/com/smartgraph/legend/base/obj/BarObj.java
+++
@@ -1,9 +0,0 @@
-package originator.ailin.com.smartgraph.legend.base.obj;
-
-/**
- * Created by Kim_Bai on 2/13/2015.
- */
-public class BarObj {
-    // For Bar Chart
-    public int width = 50, interval = 30;
-}
