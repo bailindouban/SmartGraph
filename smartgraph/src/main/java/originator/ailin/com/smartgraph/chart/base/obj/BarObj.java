741ab8d3122bbba9c979c7a7173f06d6dc3df707
bailindouban <bailindouban@gmail.com>
2015-04-09 09:40:58 +0800

    Change Package Structure

--- smartgraph/src/main/java/originator/ailin/com/smartgraph/chart/base/obj/BarObj.java
+++
@@ -1,9 +0,0 @@
-package originator.ailin.com.smartgraph.chart.base.obj;
-
-/**
- * Created by Kim_Bai on 2/13/2015.
- */
-public class BarObj {
-    // For Bar Chart
-    public int width = 50, interval = 30;
-}
